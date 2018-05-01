6782ea65902f30b28a45024c704a1e8563b74174
RobertWojtaszczyk <robert.wojtaszczyk@gmail.com>
2018-05-01 17:08:27 +0200

    JAR and PostgreSQL for Heroku fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication { 	//extends SpringBootServletInitializer {  // removed for Heroku deployment
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-    @Override
+    /*@Override	// removed for Heroku deployment
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	    return application.sources(TasksApplication.class);
-    }
+    }*/
 }
